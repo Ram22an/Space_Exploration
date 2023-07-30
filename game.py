9fd3984e0d63416af61ac5b9d7204b76ecc3b8e0
Raman Soni <soniraman2003@gmail.com>
2023-07-30 15:32:02 +0530

    Filling colors

--- game.py
+++ game.py
@@ -1,6 +1,7 @@
 # sdl=simple directmedia layer
 def game():
     import pygame as py
+    BLUE = (0, 191, 255)
     WIDTH, HEIGHT = 900, 500
     WIN = py.display.set_mode((WIDTH, HEIGHT))
     py.display.set_caption("Space_Exploration")
@@ -9,4 +10,6 @@ def game():
         for event in py.event.get():
             if event.type == py.QUIT:
                 RUN = False
+            WIN.fill(BLUE)
+            py.display.update()
     py.quit()
